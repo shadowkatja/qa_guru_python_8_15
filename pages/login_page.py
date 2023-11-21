d00c6af223201e0604f8944685f598f17a4bd774
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-11-21 14:51:22 +0300

    refactor class methods to make it more high-level

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,13 +5,12 @@ from selene.support.shared import browser
 
 class LoginPage:
 
-    def open_page(self):
+    def login(self, user: User):
         browser.open('/')
-        return self
-    def submit_login_page(self, user: User):
         browser.element('#user-name').click().type(user.username)
         browser.element('#password').click().type(user.password)
         browser.element('#login-button').click()
+        return self
 
     def login_result_standard_user(self):
         browser.element('#shopping_cart_container').should(be.visible)
