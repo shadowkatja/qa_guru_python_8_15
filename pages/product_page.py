6ffee197800c938b61462968641882a068bde544
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-11-21 14:50:53 +0300

    refactor class

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,10 +2,12 @@ from selene import be, have
 from selene.support.shared import browser
 from products.products import Product
 
-class ProductPage():
+
+class ProductPage:
 
     def open_main_page(self):
         browser.open('/inventory.html')
+
     def add_to_cart(self):
         browser.element('#add-to-cart-sauce-labs-bike-light').click()
 
@@ -18,9 +20,9 @@ class ProductPage():
     def remove_from_cart_check(self):
         browser.element('#add-to-cart-sauce-labs-bike-light').should(be.visible)
 
-
-    def check_item(self, product:Product):
+    def check_item(self, product: Product):
         browser.element(product.selector).click()
         browser.element('//div[@class="inventory_details_name large_size"]').should(have.exact_text(product.name))
-        browser.element('//div[@class="inventory_details_desc large_size"]').should(have.exact_text(product.description))
+        browser.element('//div[@class="inventory_details_desc large_size"]').should(
+            have.exact_text(product.description))
         browser.element('//div[@class="inventory_details_price"]').should(have.exact_text(product.price))
